8ac95db9f89ea5d94fa6156b2e9f324472ce5fac
Santiago Caicedo <santi-caicedo-rojas21@hotmail.com>
2022-07-30 16:10:13 -0500

    Added session :D

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from curses.ascii import isctrl
+from http.client import RemoteDisconnected
 from readline import insert_text
 from flask import Flask, request, g, redirect, url_for, render_template, session
 import uuid
 import controller
 
 app = Flask(__name__)
+app.secret_key = "SECRET"
 
 @app.route('/')
 def index():
@@ -36,8 +39,17 @@ def login_process():
 	user = request.form['username']
 	passw = request.form['password']
 	if controller.verifyLog(user, passw):
-		return redirect('/')
+		session['user'] = user
+		return redirect(url_for('user'))
 	return redirect('/error')
+
+@app.route('/user')
+def user():
+	if "user" in session:
+		user = session['user']
+		return f"<h1>Hello {user}, welcome back!</h1>"
+	else:
+		return redirect(url_for('login'))
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 5000)
